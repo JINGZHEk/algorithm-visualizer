9f2c630768bfe64400d22130a61c64fffa010c0d
Wu Jiazhe <2024006606@link.tyut.edu.cn>
2026-06-28 10:44:58 +0800

    Fix import statement in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, 网站 } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new 网站('./src', import.meta.url)),
     },
   },
   build: {
